ddcd0e53c65b70beefa53c055d622ea13ff87cfc
xiezhixian <268022625@qq.com>
2022-07-04 17:37:45 +0800

    项目初始化

---
+++ src/main/java/com/example/miu/MiuApplication.java
@@ -0,0 +1,13 @@
+package com.example.miu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/miu/MiuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
